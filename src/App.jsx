9a16c594cf7b227168dc4f46de4f7f03075d6fcd
afeezdev <afeezdev@gmail.com>
2021-08-19 19:19:58 +0100

    the UI cloning

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './app.scss';
+import Home  from './pages/home/Home';
+import Login from "./pages/login/Login"
+
+const App =() => (
+  <>
+    <Home/>
+    <Login/>
+  </>
+  )
+
+export default App;
